2e97b033fa541f9daa8f96506550dd4bdcd5e9ee
raja <raja@raja.com>
2021-08-28 00:30:12 +0530

    raja

--- Backend/database.inc.php
+++ Backend/database.inc.php
@@ -1,4 +1,13 @@
+
 <?php
+if($_SERVER['SERVER_NAME']=="localhost"){
+
 // session_start();
+// localconnection
+$connection=mysqli_connect("localhost","root","","Naamya_foundation");
+}else{
+
+// // server connection
 $connection=mysqli_connect("localhost","naamyaafoundation_db","nSsiXPc3","naamyaafoundation_db");
+}
 ?>
