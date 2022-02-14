a19bff0275201bf5d6fc0507379918658478f827
raja <raja@raja.com>
2022-02-14 17:11:25 +0530

    ald done

--- volunteer.php
+++ volunteer.php
@@ -68,96 +68,36 @@ $result_team = mysqli_query($connection, $retrive_team);
         <!--breadcrumb area end-->
 
         <!-- Newsletter cta area start-->
-        <div class=" ">
-            <div class="container">
-                <div class="back-border mrp">
+        <?php if (mysqli_num_rows($result_team) > 0) { ?>
 
-                    <h4>Key People </h4>
-                </div>
-                <div class="row team_area ">
+            <div class=" ">
+                <div class="container">
+                    <div class="back-border mrp">
 
-                    <?php while ($row = mysqli_fetch_array($result_kp)) {
-                        $description = $row['description'];
+                        <h4>Key People </h4>
+                    </div>
+                    <div class="row team_area ">
 
-                        $id = $row['id']; ?>
-                        <div class="col-xxl-3 col-xl-4 col-lg-4 col-sm-6 text-center">
+                        <?php while ($row = mysqli_fetch_array($result_kp)) {
+                            $description = $row['description'];
 
-                            <div class="single_team mb-30">
-
-                                <div class="team_thumb img_effect_white">
-
-                                    <a href="#"> <?php echo '<img class="key-img" src="data:image/jpeg;base64,' . base64_encode($row['image']) . '"/>'; ?></a>
-
-                                </div>
-                                <span class="designation"><?php echo $row['post']; ?></span>
-                                <h5 class="member_name"><a href="#"><?php echo $row['name']; ?></a> </h5>
-                                <small><?php echo substr($description, 0, 35); ?> <span class="name3 " id="show<?php echo $id; ?>">
-                                        <p class="d-flex"> <?php echo substr($description, 36); ?> </p>
-                                    </span> <?php if ($description != '') {  ?> <b class="text-danger" href="#" onclick="show('<?php echo $id; ?>')">Read
-                                            more</b> <?php } ?></small>
-                                <?php $retrive_keypople_link = "SELECT * FROM `social_media` WHERE  `reid`='$id' && `table_name`='keypepole' LIMIT 4";
-                                $result_link = mysqli_query($connection, $retrive_keypople_link); ?>
-                                <div class="member_social">
-                                    <?php while ($link_row = mysqli_fetch_array($result_link)) { ?>
-                                        <a href="<?php echo $link_row['link']; ?>" class="<?php echo $link_row['name']; ?>"><i class="fab fa-<?php echo $link_row['name']; ?>-square"></i></a>
-                                    <?php } ?>
-                                </div>
-                            </div>
-
-                        </div>
-                    <?php } ?>
-                </div>
-            </div>
-        </div>
-
-        <!-- Newsletter cta area end-->
-
-        <!-- Team area start -->
-        <div class="">
-            <div class="container">
-                <div class="back-border mrp">
-                    <h4>Our Team Member's </h4>
-                </div>
-                <div class="row team_area top-margin">
-
-                    <?php while ($row = mysqli_fetch_array($result_team)) {
-                        $id = $row['id'];
-                        $name = $row['name'];
-                        if (strpos($name, 'team') !== false) {
-                            $name = str_replace('-team', '', $name);
-                    ?>
-                            <div class="col-xxl-4 col-xl-4 col-lg-4 col-sm-6 text-center">
+                            $id = $row['id']; ?>
+                            <div class="col-xxl-3 col-xl-4 col-lg-4 col-sm-6 text-center">
 
                                 <div class="single_team mb-30">
 
                                     <div class="team_thumb img_effect_white">
-                                        <?php if (!empty($row['Image'])) { ?>
 
-                                            <a href="#"> <?php echo '<img class="img-fluid" src="data:image/jpeg;base64,' . base64_encode($row['Image']) . '"/>'; ?></a>
+                                        <a href="#"> <?php echo '<img class="key-img" src="data:image/jpeg;base64,' . base64_encode($row['image']) . '"/>'; ?></a>
 
-                                        <?php  } elseif ($row['gender'] == 'm') { ?>
-                                            <a href="#"><img class="" src=<?php echo $male; ?> alt="img"></a>
-                                        <?php } else { ?>
-                                            <a href="#"><img class="" src=<?php echo $female; ?> alt="img"></a>
-                                        <?php } ?>
                                     </div>
-
-                                    <span class="designation"><?php echo $row['department']; ?></span>
-                                    <h5 class="member_name"><a href="#"><?php echo $name; ?></a></h5>
-                                    <div class="row">
-                                        <div class="col-12">
-                                            <small><span class="name3 " id="show<?php echo $id; ?>">
-                                                    <p class="d-flex text-center"> <i class="fa fa-envelope mt-1" aria-hidden="true"></i>
-                                                        &nbsp; <?php echo ' ' . $row['email']; ?> <br> <!-- Phone : <?php //echo $row['phone']
-                                                                                                                    ?> -->
-                                                    </p>
-                                                </span><b class="text-danger" href="#" onclick="show('<?php echo $id; ?>')">Read
-                                                    more</b></small>
-                                        </div>
-                                    </div>
-
-
-                                    <?php $retrive_keypople_link = "SELECT * FROM `social_media` WHERE  `reid`='$id' && `table_name`='volunteer' LIMIT 3";
+                                    <span class="designation"><?php echo $row['post']; ?></span>
+                                    <h5 class="member_name"><a href="#"><?php echo $row['name']; ?></a> </h5>
+                                    <small><?php echo substr($description, 0, 35); ?> <span class="name3 " id="show<?php echo $id; ?>">
+                                            <p class="d-flex"> <?php echo substr($description, 36); ?> </p>
+                                        </span> <?php if ($description != '') {  ?> <b class="text-danger" href="#" onclick="show('<?php echo $id; ?>')">Read
+                                                more</b> <?php } ?></small>
+                                    <?php $retrive_keypople_link = "SELECT * FROM `social_media` WHERE  `reid`='$id' && `table_name`='keypepole' LIMIT 4";
                                     $result_link = mysqli_query($connection, $retrive_keypople_link); ?>
                                     <div class="member_social">
                                         <?php while ($link_row = mysqli_fetch_array($result_link)) { ?>
@@ -167,11 +107,77 @@ $result_team = mysqli_query($connection, $retrive_team);
                                 </div>
 
                             </div>
-                    <?php }
-                    } ?>
+                        <?php } ?>
+                    </div>
                 </div>
             </div>
-        </div>
+        <?php } ?>
+        <!-- Newsletter cta area end-->
+
+        <!-- Team area start -->
+        <?php if (mysqli_num_rows($result_team) > 0) { ?>
+
+            <div class="">
+                <div class="container">
+                    <div class="back-border mrp">
+                        <h4>Our Team Member's </h4>
+                    </div>
+                    <div class="row team_area top-margin">
+
+                        <?php while ($row = mysqli_fetch_array($result_team)) {
+                            $id = $row['id'];
+                            $name = $row['name'];
+                            if (strpos($name, 'team') !== false) {
+                                $name = str_replace('-team', '', $name);
+                        ?>
+                                <div class="col-xxl-4 col-xl-4 col-lg-4 col-sm-6 text-center">
+
+                                    <div class="single_team mb-30">
+
+                                        <div class="team_thumb img_effect_white">
+                                            <?php if (!empty($row['Image'])) { ?>
+
+                                                <a href="#"> <?php echo '<img class="img-fluid" src="data:image/jpeg;base64,' . base64_encode($row['Image']) . '"/>'; ?></a>
+
+                                            <?php  } elseif ($row['gender'] == 'm') { ?>
+                                                <a href="#"><img class="" src=<?php echo $male; ?> alt="img"></a>
+                                            <?php } else { ?>
+                                                <a href="#"><img class="" src=<?php echo $female; ?> alt="img"></a>
+                                            <?php } ?>
+                                        </div>
+
+                                        <span class="designation"><?php echo $row['department']; ?></span>
+                                        <h5 class="member_name"><a href="#"><?php echo $name; ?></a></h5>
+                                        <div class="row">
+                                            <div class="col-12">
+                                                <small><span class="name3 " id="show<?php echo $id; ?>">
+                                                        <p class="d-flex text-center"> <i class="fa fa-envelope mt-1" aria-hidden="true"></i>
+                                                            &nbsp; <?php echo ' ' . $row['email']; ?> <br> <!-- Phone : <?php //echo $row['phone']
+                                                                                                                        ?> -->
+                                                        </p>
+                                                    </span><b class="text-danger" href="#" onclick="show('<?php echo $id; ?>')">Read
+                                                        more</b></small>
+                                            </div>
+                                        </div>
+
+
+                                        <?php $retrive_keypople_link = "SELECT * FROM `social_media` WHERE  `reid`='$id' && `table_name`='volunteer' LIMIT 3";
+                                        $result_link = mysqli_query($connection, $retrive_keypople_link); ?>
+                                        <div class="member_social">
+                                            <?php while ($link_row = mysqli_fetch_array($result_link)) { ?>
+                                                <a href="<?php echo $link_row['link']; ?>" class="<?php echo $link_row['name']; ?>"><i class="fab fa-<?php echo $link_row['name']; ?>-square"></i></a>
+                                            <?php } ?>
+                                        </div>
+                                    </div>
+
+                                </div>
+                        <?php }
+                        } ?>
+                    </div>
+                </div>
+            </div>
+
+        <?php } ?>
         <!-- Team area end -->
 
         <!-- Team area start -->
@@ -189,31 +195,31 @@ $result_team = mysqli_query($connection, $retrive_team);
                             $id = $row['id'];
                             $name = $row['name'];
                         ?>
-                                <div class="col-xxl-3 col-xl-3 col-lg-4 col-sm-6 text-center">
+                            <div class="col-xxl-3 col-xl-3 col-lg-4 col-sm-6 text-center">
 
-                                    <div class="single_team mb-30">
+                                <div class="single_team mb-30">
 
-                                        <div class="team_thumb ">
-                                            <?php if ($row['gender'] == 'm') { ?>
-                                                <a href="#"><img class="img-fluid" src=<?php echo $male; ?> alt="img"></a>
-                                            <?php } else { ?>
-                                                <a href="#"><img class="img-fluid" src=<?php echo $female; ?> alt="img"></a>
-                                            <?php } ?>
-                                        </div>
-                                        <span class="designation"><?php echo $row['department']; ?></span>
-                                        <h5 class="member_name"><a href="#"><?php echo $name; ?></a></h5>
-                                        <?php $retrive_keypople_link = "SELECT * FROM `social_media` WHERE  `reid`='$id' && `table_name`='volunteer' LIMIT 3";
-                                        $result_link = mysqli_query($connection, $retrive_keypople_link); ?>
-                                        <div class="member_social">
-                                            <?php while ($link_row = mysqli_fetch_array($result_link)) { ?>
-                                                <a href="<?php echo $link_row['link']; ?>" class="<?php echo $link_row['name']; ?>"><i class="fab fa-<?php echo $link_row['name']; ?>-square"></i></a>
-                                            <?php } ?>
-                                        </div>
+                                    <div class="team_thumb ">
+                                        <?php if ($row['gender'] == 'm') { ?>
+                                            <a href="#"><img class="img-fluid" src=<?php echo $male; ?> alt="img"></a>
+                                        <?php } else { ?>
+                                            <a href="#"><img class="img-fluid" src=<?php echo $female; ?> alt="img"></a>
+                                        <?php } ?>
                                     </div>
-
+                                    <span class="designation"><?php echo $row['department']; ?></span>
+                                    <h5 class="member_name"><a href="#"><?php echo $name; ?></a></h5>
+                                    <?php $retrive_keypople_link = "SELECT * FROM `social_media` WHERE  `reid`='$id' && `table_name`='volunteer' LIMIT 3";
+                                    $result_link = mysqli_query($connection, $retrive_keypople_link); ?>
+                                    <div class="member_social">
+                                        <?php while ($link_row = mysqli_fetch_array($result_link)) { ?>
+                                            <a href="<?php echo $link_row['link']; ?>" class="<?php echo $link_row['name']; ?>"><i class="fab fa-<?php echo $link_row['name']; ?>-square"></i></a>
+                                        <?php } ?>
+                                    </div>
                                 </div>
+
+                            </div>
                         <?php }
-                         ?>
+                        ?>
                     </div>
                 </div>
             </div>
